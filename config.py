6855ac6accfce016dc3dc3e94680ffaecc8215c9
ishuishere <130059119+ishuishere@users.noreply.github.com>
2024-03-28 19:09:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID =   # api id
-API_HASH = ""  # api hash
+API_ID = 24764967 # api id
+API_HASH = "48a5c7881e9fd7ff8981538602144672"  # api hash
 
-BOT_TOKEN = ""  # bot token
+BOT_TOKEN = "7035902656:AAE2-es67OzM3QkwNomaTaWksHbZu-LT9y8"  # bot token
 
 
 ## REDIS
-HOST = ""  # redis host uri
+HOST = "redis-15731.c302.asia-northeast1-1.gce.cloud.redislabs.com"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "15731"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
-ADMINS = [1317173146]
+PRIVATE_CHAT_ID = -1002143112715  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "csrfToken=m2txVlT7g4XpQ82zR2qtrEs2; browserid=CHbDjJrJENbgAK4sGGo3J3i81lZFJNWGk9bKxo9bDk4YY8Lc-v9gwZsNhmI=; lang=en; TSID=8hSzyk2a7jgfYKgOr65t9QB4l17djTuF; __bid_n=18e8546de8cc1d50544207; ndut_fmt=CE1647110222E55303A4FFF0E2ABFE68627CB6876A5DD4536B1D36691DBB38E0; _ga=GA1.1.1434759955.1711633019; _ga_06ZNKL8C2E=GS1.1.1711633018.1.0.1711633050.28.0.0"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
+ADMINS = [1528506619]
